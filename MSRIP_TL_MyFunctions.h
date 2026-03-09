ebb06be2f5d16d3f640abe07f52ac7c2a87355a5
Lyssandre Marchand <lylymarchand@gmail.com>
2026-03-09 09:49:43 +0100

    Fix syntax error in GAMETE_PROD function

--- MSRIP_TL_MyFunctions.h
+++ MSRIP_TL_MyFunctions.h
@@ -86,7 +86,7 @@ void FITNESS_LANDSCAPE_BM(const double sa, const double ha, const double sb, con
 void GAMETE_PROD(const double dip_IND[10], const double Me_Mu_Matrix[][4], double* hap_FREQ)
 {
 	for (int k = 0; k < 4; ++k) { // Remise à 0 car on store avec += donc risque d'accumulation
-		hap_FREQ[k] = 0
+		hap_FREQ[k] = 0;
 			}
 	
 	for (int i(0); i < 10; ++i) {
