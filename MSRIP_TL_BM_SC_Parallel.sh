3c195466cb57f727b0dc7fe14e4e2521717a7d2f
Lyssandre Marchand <lylymarchand@gmail.com>
2026-03-11 10:20:52 +0100

    Cleaning script

--- MSRIP_TL_BM_SC_Parallel.sh
+++ MSRIP_TL_BM_SC_Parallel.sh
@@ -30,8 +30,8 @@ echo "Simulations starting..."
 
 for self_r_1 in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
   for self_r_2 in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
-    for mu_1 in 0; do
-      for mu_2 in 0; do
+    for mu_1 in 0; do // No mutation because fixed
+      for mu_2 in 0; do // No mutation because fixed 
         for ha_1 in 0.5; do
           for sa_1 in 0; do
             for hb_1 in 0.5; do
@@ -42,7 +42,7 @@ for self_r_1 in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
                       for hb_2 in 0.5; do
                         for sb_2 in 0.5; do
                           for gamma_2 in 0.1; do
-                            for rec_1 in 0.5; do
+                            for rec_1 in 0; do // No recombinasion because fixed
                               for rec_2 in 0.5; do
                                 for m_h_1 in 0; do
                                   for m_h_2 in 0; do
@@ -78,7 +78,9 @@ done
 echo "Simulations terminées !"
 
 # CSV
-printf "threshold,iteration,span,interval,self_r_1,self_r_2,ha_1,sa_1,hb_1,sb_2,epsilon_aa_1, epsilon_ad_1, epsilon_dd_1,ha_2,sa_2,hb_2,sb_2,gamma_2,rec_2,m_h_1,m_h_2,m_d,gen," > ColumnHeader_TL_BM_SC.csv
+printf "threshold,iteration,span,interval,self_r_1,self_r_2," >> ColumnHeader_TL_BM_SC.csv
+printf "ha_2,sa_2,hb_2,sb_2,gamma_2,rec_2," >> ColumnHeader_TL_BM_SC.csv
+printf "m_h_1,m_h_2,m_d,gen," >> ColumnHeader_TL_BM_SC.csv
 printf "A_FREQ_1,a_FREQ_1,B_FREQ_1,b_FREQ_1,A_FREQ_2,a_FREQ_2,B_FREQ_2,b_FREQ_2" >> ColumnHeader_TL_BM_SC.csv
 printf "AB/AB_0,AB/Ab_0,AB/aB_0,AB/ab_0,Ab/Ab_0,Ab/aB_0,Ab/ab_0,aB/aB_0,aB/ab_0,ab/ab_0_pop1," >> ColumnHeader_TL_BM_SC.csv
 printf "AB/AB_0,AB/Ab_0,AB/aB_0,AB/ab_0,Ab/Ab_0,Ab/aB_0,Ab/ab_0,aB/aB_0,aB/ab_0,ab/ab_0_pop2" >> ColumnHeader_TL_BM_SC.csv
