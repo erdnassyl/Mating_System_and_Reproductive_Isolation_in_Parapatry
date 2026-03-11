c2962622bdd519f6f3672fed8986d624024242db
Lyssandre Marchand <lylymarchand@gmail.com>
2026-03-11 15:52:50 +0100

    Update MSRIP_TL_BM_SC_Parallel.sh

--- MSRIP_TL_BM_SC_Parallel.sh
+++ MSRIP_TL_BM_SC_Parallel.sh
@@ -38,7 +38,7 @@ for self_r_1 in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
               for sb_1 in 0; do
                 for gamma_1 in 0; do
                   for ha_2 in 0.5; do
-                    for sa_2 in 0.1; do
+                    for sa_2 in 0; do
                       for hb_2 in 0.5; do
                         for sb_2 in 0; do
                           for gamma_2 in -0.0025; do
